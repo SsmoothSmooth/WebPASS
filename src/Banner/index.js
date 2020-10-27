9c4f47dc320a2848f159fb72b60f5d51571f1555
José Wilson de Lima <josewilson791@gmail.com>
2020-10-27 20:36:32 -0300

    Criando banner

---
+++ src/Banner/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png'
+import './banner.css'
+
+function Content(){
+    return(
+        
+        <section>          
+            <img className="" src={} alt="" />
+        
+            <p>
+                
+            </p>
+        </section>
+    );
+}
+
+export default Content;
