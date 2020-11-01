6d76177c54c301a2312ccdd11ba758844d90f2e8
José Wilson de Lima <josewilson791@gmail.com>
2020-10-31 23:37:29 -0300

    Criando footer das página

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import  './Footer.css';
 
 function Footer() {
     return (
-        <>
-            <h1> Colocando footer</h1>
-        </>
+        <div className="footer">
+            <h1>PASS</h1>
+            <p>Destinos inspiradores ao seu alcance</p>
+            <p>Protótipo desenvolvido por:</p>
+            <ul className="titulo">
+                <li><p>&#128374; Andre</p></li>
+                <li><p>&#128170; Matheus</p></li>
+                <li><p>&#128295; José Wilson de Lima</p></li>
+            </ul>
+        </div>
     );
 }
 
